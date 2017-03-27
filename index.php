58c1bff3ec4f8e7954278b9214b1330d9f077d5d
StateMonster <tannerrobinaugh@gmail.com>
2017-03-27 00:24:45 -0400

    Readded Carly's changes

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Cyber Training Hub</title>
+        <meta name = "description" content="Air Force Cyber training hub">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/home.css"> 
+        <script src="https://code.jquery.com/jquery-3.1.1.min.js"
+        integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+        crossorigin="anonymous"></script>
+        <script src="scripts/main.js"></script>
+    </head>
+    <body>
+        <header>
+            <h1>Cyber Training Hub</h1>
+            <nav>
+                <ul>
+                    <li class = "homeop"><a href="index.html">| Home |</a></li>
+                    <li><a href="login.php">| Log in |</a></li>
+                    <li><a href="search.php">| Search |</a></li>
+                    <li><a href="about.html">| About |</a></li>
+                </ul>
+            </nav>
+    
+        </header>
+
+        <main> 
+            <img src = "images/AirForceCyberSecurity.jpg">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi elementum sapien dui. Nullam est magna, 
+            tristique sagittis elit sit amet, sodales sollicitudin nunc. Nullam dapibus neque vitae orci hendrerit, et
+            interdum lacus mollis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam turpis libero, pellentesque 
+            et vestibulum eget, dapibus ut sem. Quisque et lorem a odio malesuada maximus. In consequat ut sem et auctor.</p>
+            <p>Cras et erat vestibulum, imperdiet lectus in, cursus lorem. Sed varius accumsan nulla, eget luctus erat elementum 
+            in. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sit amet ante eget est euismod molestie. Phasellus 
+            nec faucibus enim, a lobortis sapien. Nunc quis metus at odio congue commodo. Morbi rutrum nunc sem, non ultricies massa 
+            vehicula id. Aenean non felis risus. Donec scelerisque, libero ac congue cursus, ipsum quam maximus elit, et porttitor
+            nibh neque ut libero. Etiam mollis ultrices leo vitae hendrerit. Quisque nec congue odio. Sed purus velit, pellentesque 
+            at semper vel, auctor vel nunc.</p>
+            <p>Aenean feugiat nibh nunc, quis maximus nisl consectetur non. Fusce imperdiet arcu vestibulum efficitur facilisis. 
+            Nullam aliquet orci ut sapien pellentesque, mollis tincidunt nulla blandit. Proin sagittis mattis lacus a semper. 
+            Maecenas et tortor eget lacus gravida convallis. Aenean feugiat eget libero quis volutpat. Pellentesque varius 
+            lacus vitae nibh consequat aliquam. Mauris tincidunt auctor dolor, sed congue augue commodo in. Fusce sit amet ex 
+            eget nibh faucibus pharetra a sed magna. Donec vel condimentum velit, eu luctus nulla.</p> 
+        </main>
+  
+        <footer>
+          <p>Footer</p>
+        </footer>
+    </body>
+</html>
---
+++ login.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>Cyber Training Hub</title>
+        <meta charset="utf-8">
+        <meta name = "description" content="Air Force Cyber training hub">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">    
+        <link rel="stylesheet" href="css/login.css">   
+        <?php require_once 'PHP/login.php';?>
+        <script
+          src="https://code.jquery.com/jquery-3.1.1.min.js"
+          integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+          crossorigin="anonymous"></script>
+
+        <script src="scripts/main.js"></script>  
+    </head>
+    <header>
+     <?php connect("localhost", "root", "");?>
+     <img src = "images/home.png" alt="Home">
+     
+     <h1>Cyber Training Hub</h1>
+     
+     <nav>
+       <ul>
+         <li><a href="index.html">| Home |</a></li>
+         <li><a href="login.html" id = "login">| Log in |</a></li>
+         <li><a href="search.html">| Search |</a></li>
+         <li><a href="about.html">| About |</a></li>
+       </ul>
+     </nav>
+     
+   </header>
+ 
+   <main>  
+     <h2>Log into your account</h2>
+     <form action="" method = "post"><!-------------------------------------------------------------THIS NEEDS DONE------------------------------------->
+     <p>
+ 			<label for="uname" id="name">Username</label>
+ 			<input type="text" name="uname" required><br><br>
+ 			<label for="pass" id="passw">Password</label>
+ 			<input type="password" name="pass" required><br>
+ 		</p>
+     <input type = "Button" value = "Create New Account"><!------------------------------ADD ACTION------------------------------------->
+     <input type="submit" value="Submit" name = "login">
+ </form>
+   </main>
+   
+   <footer>
+     <p>Footer</p>
+   </footer>
+ 
+   </body>
+</html>
